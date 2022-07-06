47809bf7ccd9606dc6af8fbeaf2a90742c418461
MarcosOliveir4 <marcosassis071@gmail.com>
2022-07-06 08:48:50 -0300

    :white_check_mark: refactor validate cpf

--- projeto-parte-01/src/util/cpf/cpf.spec.ts
+++ projeto-parte-01/src/util/cpf/cpf.spec.ts
@@ -1,22 +1,47 @@
 import { validate } from "./cpf";
 
+const wrongSameDigitCpf = [
+    "111.111.111-11",
+    "222.222.222-22",
+    "333.333.333-33",
+];
+
 describe("Valida CPF", () => {
-    it("Deve retornar true se o CPF é valido com mascara", () => {
-        expect(validate("090.493.373-33")).toBe(true);
+    it("Deve validar um CPF válido", () => {
+        const isValid = validate("96299048026");
+        expect(isValid).toBeTruthy();
     });
-    it("Deve retornar true se o CPF é valido sem mascara", () => {
-        expect(validate("09049337333")).toBe(true);
+
+    it("Deve validar um CPF válido sem pontos e traçoes", () => {
+        const isValid = validate("962.990.480-26");
+        expect(isValid).toBeTruthy();
     });
-    it("Deve retornar false se o CPF é invalido", () => {
-        expect(validate("090.493.373-3dsad3")).toBe(false);
+
+    it("Deve validar um CPF válido com alguns pontos", () => {
+        const isValid = validate("962.990.48026");
+        expect(isValid).toBeTruthy();
     });
-    it("Deve retornar false se passar null como parametro", () => {
-        expect(validate(null)).toBe(false);
+
+    it.each(wrongSameDigitCpf)(
+        "Deve validar um CPF inválido com todos os números iguais",
+        (cpf) => {
+            const isValid = validate(cpf);
+            expect(isValid).toBeFalsy();
+        }
+    );
+
+    it("Deve validar uma string vazia", () => {
+        const isValid = validate("");
+        expect(isValid).toBeFalsy();
     });
-    it("Deve retornar false se passar undefined como parametro", () => {
-        expect(validate(undefined)).toBe(false);
+
+    it("Deve validar um CPF com menos caracteres", () => {
+        const isValid = validate("22");
+        expect(isValid).toBeFalsy();
     });
-    it("Deve retornar false se o CPF estive com o digito invalido", () => {
-        expect(validate("090.493.373-44")).toBe(false);
+
+    it("Deve validar um CPF com o digito 0", () => {
+        const isValid = validate("58181681070");
+        expect(isValid).toBeTruthy();
     });
 });
